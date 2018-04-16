c832de1696db2df043e2a9036772dfeb3232ad6d
sdeleuze <sdeleuze@pivotal.io>
2018-04-16 15:00:35 +0200

    Remove unused imports

--- src/test/kotlin/com/example/FooIntegrationTests.kt
+++ src/test/kotlin/com/example/FooIntegrationTests.kt
@@ -8,7 +8,6 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.reactive.server.WebTestClient
-import org.springframework.test.web.reactive.server.returnResult
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
